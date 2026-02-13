6967e09bc7e31ff0eb1b8449bf5c37074ff47f71
Mattia Matteini <matteinimattia@gmail.com>
2026-02-13 16:29:03 +0100

    fix: exclude _index.md causing build loop when file is mounted in a container

--- serve.sh
+++ serve.sh
@@ -4,6 +4,6 @@ trap "kill 0" EXIT
 shared-slides/preprocess.rb
 hugo server $HUGO_ARGS &
 while [ -e /proc/$! ]; do
-    inotifywait -e modify,create,delete -r content shared-slides && sleep 1 && shared-slides/preprocess.rb;
+    inotifywait -e modify,create,delete -r --exclude '_index\.md' content shared-slides && sleep 1 && shared-slides/preprocess.rb;
 done &
 wait
